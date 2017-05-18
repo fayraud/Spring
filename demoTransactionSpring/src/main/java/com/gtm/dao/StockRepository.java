d974cd4cc336f959862d653bc036acabc57cf93b
fayraud <florent.ayraud@gmail.com>
2017-05-18 16:07:59 +0200

    annotation transactional

--- demoTransactionSpring/src/main/java/com/gtm/dao/StockRepository.java
+++ demoTransactionSpring/src/main/java/com/gtm/dao/StockRepository.java
@@ -3,9 +3,11 @@ package com.gtm.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gtm.metier.Stock;
 
+@Transactional
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 	
 	@Modifying
