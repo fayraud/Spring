d90b12882ae9b8a67a21f8c31a3ea0fcc35fc004
fayraud <florent.ayraud@gmail.com>
2017-05-18 15:57:05 +0200

    anotation metier

--- demoTransactionSpring/src/main/java/com/gtm/metier/Stock.java
+++ demoTransactionSpring/src/main/java/com/gtm/metier/Stock.java
@@ -1,7 +1,19 @@
 package com.gtm.metier;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ValueGenerationType;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
 public class Stock {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String nom;
 	private int quantité;
