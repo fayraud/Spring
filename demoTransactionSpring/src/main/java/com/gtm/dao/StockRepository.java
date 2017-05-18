93a27e609d8a2712627cad1d9106b8afe50c6e01
fayraud <florent.ayraud@gmail.com>
2017-05-18 15:58:30 +0200

    annotation dao

--- demoTransactionSpring/src/main/java/com/gtm/dao/StockRepository.java
+++ demoTransactionSpring/src/main/java/com/gtm/dao/StockRepository.java
@@ -1,13 +1,14 @@
 package com.gtm.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.gtm.metier.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 	
-	
+	@Modifying
 	@Query("update Stock s set s.quantite=s.quantite-?2 where s.id=?1")
 	Void sortArcticleDuStock(int id, int qute);
 	
