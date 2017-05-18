a22c50993c0ab2cdb9673e112aecceabe445fadf
fayraud <florent.ayraud@gmail.com>
2017-05-18 14:52:04 +0200

    creation et implementation de la classe metier Stock

---
+++ TpTransactionsGestionStockSpringData/src/main/java/com/gtm/metier/Stock.java
@@ -0,0 +1,33 @@
+package com.gtm.metier;
+
+public class Stock {
+
+	private int id;
+	private String nom;
+	private int quantité;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getQuantité() {
+		return quantité;
+	}
+
+	public void setQuantité(int quantité) {
+		this.quantité = quantité;
+	}
+
+}
