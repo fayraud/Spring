712ad587a79b53ea230c3d35c946ca0a1093167a
fayraud <florent.ayraud@gmail.com>
2017-05-18 15:19:46 +0200

    creation interface IService et implementation

---
+++ demoTransactionSpring/src/main/java/service/IService.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface IService {
+	
+	public void sortArticleDuStock(int id, int qute) throws
+	articleNotFoundException, QteNegativeException, NotEnoughArticleException;
+
+}
