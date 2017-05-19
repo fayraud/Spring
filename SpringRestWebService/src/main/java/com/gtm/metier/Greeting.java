9bcca3e23d8a54ef683d287a949d5255b609c7e9
fayraud <florent.ayraud@gmail.com>
2017-05-19 10:26:20 +0200

    creation de greeton controller et changement de dependence

--- SpringRestWebService/src/main/java/com/gtm/metier/Greeting.java
+++ SpringRestWebService/src/main/java/com/gtm/metier/Greeting.java
@@ -1,26 +1,34 @@
 package com.gtm.metier;
 
 public class Greeting {
-	
+
 	private long id;
-    private String content;
+	private String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	@Override
 	public String toString() {
 		return "Greeting [id=" + id + ", content=" + content + "]";
 	}
-
-    
 
 }
